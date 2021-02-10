eb9710fd32e2d122ce3f0ee9e2a12fc9f459fc94
indranil2507 <iindranilsaha1@gmail.com>
2021-02-10 12:42:07 +0530

    order functions

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import {createStore, applyMiddleware,combineReducers, compose } from "redux";
 import thunk from "redux-thunk";
 import {productsReducer} from "./reducers/productReducers";
 import {cartReducer} from "./reducers/cartReducers"
+import { orderReducer } from "./reducers/orderReducer";
 
 const initialState={};
 const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE|| compose;
@@ -9,7 +10,8 @@ const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE|| compose;
 const store= createStore(
     combineReducers({
         products: productsReducer,
-        cart:cartReducer
+        cart:cartReducer,
+        order:orderReducer
     }),
     initialState,
     
